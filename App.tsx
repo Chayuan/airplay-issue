9b26ac410d43b99f13a8abc1cf4731c6ed2e9058
Chayuan <gwenael.rob@gmail.com>
2023-06-14 17:31:43 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import {StyleSheet, Text, View, Alert, TouchableOpacity} from 'react-native';
+
+export default function App() {
+  function onPress() {
+    Alert.alert('Title', 'Message', [
+      {
+        text: 'Yes',
+        onPress: () => {
+          console.log('Yes');
+        },
+      },
+      {
+        text: 'No',
+        style: 'cancel',
+      },
+    ]);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity
+        onPress={onPress}
+        style={{height: 50, width: 150, backgroundColor: 'lavender'}}>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
